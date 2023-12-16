a43f47ce9584a6a60c515cf4159ed4f0a8e98ac0
Jacky <sjyanxin@126.com>
2023-12-16 22:36:54 +0800

    add klayn chain

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -10,12 +10,20 @@ import {
   cronos,
   eos,
   fantom,
+  filecoin,
   gnosis,
+ iotex, 
+ kava,
+  klaytn,
   linea,
   mainnet,
+  moonbeam,
+  neonMainnet,
   okc,
   opBNB,
+  optimism,
   polygon,
+  sepolia,
   zkSync} from "viem/chains";
 
 export const shibarium = defineChain({
@@ -38,6 +46,30 @@ export const shibarium = defineChain({
   blockExplorers: {
     default: { name: "Explorer", url: "https://shibariumscan.io" },
   },
+});
+
+export const ethw = defineChain({
+  id: 10001,
+  network: 'EthereumPoW',
+  name: 'EthereumPoW',
+  nativeCurrency: { name: 'ethw', symbol: 'ETHW', decimals: 18 },
+  rpcUrls: { 
+     default: { http: ['https://mainnet.ethereumpow.org'] },
+    public: {      
+      http: ['https://mainnet.ethereumpow.org'],
+    },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'Etherscan',
+      url: 'https://mainnet.ethwscan.com',
+    },
+    default: {
+      name: 'Etherscan',
+      url: 'https://www.oklink.com/en/ethw',
+    },
+  },
+ 
 });
 
 export const cfx = defineChain({
@@ -68,23 +100,29 @@ export const cfx = defineChain({
 export const inscriptionChains = {
   eth: mainnet,
   bsc,
+  opBNB,
+  okc,
   polygon,
+  fantom,
   avalanche,
   classic,
+  klaytn,
+  optimism,
   base,
   arbitrum,
   zkSync,
   eos,
   linea,
-  okc,
-  fantom,
-  opBNB,
+  
   celo,
   confluxESpace,
   gnosis,
+  filecoin,
   shibarium,
-  cfx,
-  cronos
+  sepolia,
+  moonbeam,
+  iotex,
+  kava,ethw
 };
 
 export type ChainKey = keyof typeof inscriptionChains;
